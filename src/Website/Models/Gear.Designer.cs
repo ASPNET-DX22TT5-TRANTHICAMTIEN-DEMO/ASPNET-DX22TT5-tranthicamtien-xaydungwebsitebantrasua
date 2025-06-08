ce5f73d7aef3153051d2528c44d3a04463c878e7
tranthicamtien <tranthicamtien2801@gmail.com>
2025-06-08 19:59:06 +0700

    hoan thanh bai do an ngay 08/06/2025

--- src/Website/Models/Gear.Designer.cs
+++ src/Website/Models/Gear.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Tien\Desktop\asp\ASPNET-DX22TT5-tranthicamtien-xaydungwebsitebantrasua\src\Website\Models\Gear.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Desktop\Springboot\trasua\Website\Models\Gear.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
